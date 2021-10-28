6ba679809a628fcf8086629920fb00b8050061e9
HanneMaes <Maes.hanne@gmail.com>
2021-10-28 09:48:40 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -9,9 +9,9 @@ function documentReady() {
     console.log('Key pressed:', key)
 
     // redirect
-    if     (key == 'W') { window.location.replace("https://bazandpoort.smartschool.be/"); }         // w = zandpoort
-    else if(key == 'P') { window.location.replace("https://bapitzemburg.smartschool.be/"); }        // p = pitzemburg
-    else if(key == 'D') { window.location.replace("https://drive.google.com/drive/u/1/my-drive"); } // d = drive
+    if     (key == 'W') { window.location.replace("https://bazandpoort.smartschool.be/"); }                                          // w = zandpoort
+    else if(key == 'P') { window.location.replace("https://bapitzemburg.smartschool.be/"); }                                         // p = pitzemburg
+    else if(key == 'D') { window.location.replace("https://drive.google.com/drive/u/0/folders/1AuZJmhj_dJzCn33X3rmpsi9Edi3hf2I1"); } // d = drive
   }
 
 }
